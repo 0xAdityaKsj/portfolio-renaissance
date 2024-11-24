a8bdefce21f89646b9add6646fb4f6e95707a3f6
Sai Jyothir Aditya Kantipudi <106384725+0xAdityaKsj@users.noreply.github.com>
2024-11-24 13:47:43 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
-
 export default function Home() {
   return (
     <div className="bg-paper">
       <main className="container">
-        <section>
-          <p>
-            <span className="first-letter">W</span>
-            elcome to my corner of the digital world. Here you'll find my thoughts,
-            works, and explorations in the field of...
-          </p>
-        </section>
+        <div className="card-container">
+          <div className="card">
+            <div className="card-content">
+              <p>
+                <span className="first-letter">W</span>
+                elcome to my corner of the digital world. Here you'll find my thoughts,
+                works, and explorations in the field of...
+              </p>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   )
